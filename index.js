311cefa286dda197a0d15c2c9f9b945785e7c786
mikinty <youmichaelc@gmail.com>
2020-07-24 00:44:19 -0400

    new site with links

--- index.js
+++ index.js
@@ -3,25 +3,76 @@
  */
 
 function build_site () {
-  let green_flash = document.createElement('div');
-  let red_flash   = document.createElement('div');
-  let text_box    = document.createElement('div');
+  // Where we put the logo and title
+  let main_page = document.createElement('div');
+  let title_box = document.createElement('div');
+  let links = document.createElement('div');
+  let text_box = document.createElement('div');
 
-  green_flash.classList.add('green_flash');
-  red_flash.classList.add('red_flash');
   text_box.classList.add('text_box');
 
-  // document.body.appendChild(green_flash);
-  // document.body.appendChild(red_flash);
-  document.body.appendChild(text_box);
+  document.body.appendChild(main_page);
+  main_page.appendChild(title_box);
+
+  main_page.classList.add('main_page');
+
+  // Title
+  let logo_link = document.createElement('a');
+  logo_link.href = 'https://www.linkedin.com/company/66927992';
+  let logo = document.createElement('img');
+  logo.src = './assets/viridity_logo_small.png';
+  const LOGO_SIZE = 140;
+  logo.width = LOGO_SIZE;
+  logo.height = LOGO_SIZE;
+
+  let title_text = document.createElement('div');
+  title_text.innerHTML = 'Viridity Capital, LLC';
+
+  logo_link.appendChild(logo);
+  title_box.appendChild(logo_link);
+  title_box.appendChild(title_text);
+
+  title_box.classList.add('title_box');
+
+  let motto = document.createElement('div');
+  motto.innerHTML = '#bearlivesmatter';
+  motto.classList.add('motto');
+  main_page.appendChild(motto);
+
+  // Links
+  main_page.appendChild(links);
+  links.classList.add('links');
+  links.innerHTML = 
+  `
+  <table>
+  <tr>
+  <td>
+  <a href="https://viriditycapital.github.io/theta">theta</a>
+  </td>
+  <td>
+  burn options premiums 🔥
+  </td>
+  </tr>
+  <tr>
+  <td>
+  <a href="https://viriditycapital.github.io/rpt/">rpt</a>
+  </td>
+  <td>
+  weighted rice purity test
+  </td>
+  </tr>
+  </table>
+  `;
+
+  main_page.appendChild(text_box);
 
   // We hide one of the flashing backgrounds
   if (Math.random() > 0.5) {
-    text_box.innerHTML = 'SPY TO THE FLOOR';
-    document.body.classList.add('red_flash');
+    text_box.innerHTML = 'SPY TO <br> THE FLOOR';
+    text_box.classList.add('red_flash');
   } else {
-    text_box.innerHTML = 'SPY TO THE MOON';
-    document.body.classList.add('green_flash');
+    text_box.innerHTML = 'SPY TO <br> THE MOON';
+    text_box.classList.add('green_flash');
   }
 }
 
