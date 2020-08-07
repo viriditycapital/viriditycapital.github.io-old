9c360db4c6033a9737cbff7c49d4f2fc9827a51a
mikinty <youmichaelc@gmail.com>
2020-08-06 23:55:55 -0400

    Touch support

--- src/index.js
+++ src/index.js
@@ -247,8 +247,66 @@ function build_site () {
     page_contact
   ];
 
+  // Keep track of scrolling
   var curr_page_idx = 0;
   const NUM_PAGES = PAGES.length;
+
+  // Detect touch on mobile, taken from
+  // https://stackoverflow.com/questions/2264072/detect-a-finger-swipe-through-javascript-on-the-iphone-and-android
+  document.addEventListener('touchstart', handleTouchStart, false);        
+  document.addEventListener('touchmove', handleTouchMove, false);
+
+  var xDown = null;                                                        
+  var yDown = null;
+
+  function getTouches(evt) {
+    return evt.touches;
+  }                                                     
+
+  function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+  }
+
+  function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+      return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+      if ( xDiff > 0 ) {
+        /* left swipe */ 
+      } else {
+        /* right swipe */
+      }                       
+    } else {
+      if ( yDiff > 0 ) {
+        /* up swipe */ 
+        curr_page_idx = Math.min(NUM_PAGES - 1, curr_page_idx + 1);
+      } else { 
+        /* down swipe */
+        curr_page_idx = Math.max(0, curr_page_idx - 1);
+      }                                                                 
+
+      if (curr_page_idx > 0) {
+        toolbar.classList.remove('hidden');
+      } else {
+        toolbar.classList.add('hidden');
+      }
+
+      window.scrollTo(0, PAGES[curr_page_idx].offsetTop);
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+  }
 
   document.addEventListener('mousewheel', _.debounce((e) => { 
     let deltaY = e.wheelDeltaY;
